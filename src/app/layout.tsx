dd491175e888471e6a013b5147c08cade62b111f
mrrobottic <142896094+mrrobottic@users.noreply.github.com>
2025-08-24 09:01:59 +0100

    google update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
 import Script from 'next/script';
+import { GoogleAnalytics } from '@next/third-parties/google'
+
 
 export const metadata: Metadata = {
   title: 'TRAKMINT',
@@ -19,18 +21,12 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Lexend:wght@400;500;600;700&display=swap" rel="stylesheet"></link>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-X5Z4SVRHV3"></script>
-        <script>
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-X5Z4SVRHV3');`}
-        </script>
+     
       </head>
       <body className="font-body antialiased">
         {children}
         <Toaster />
+        <GoogleAnalytics gaId="G-X5Z4SVRHV3" />
         <script src="https://analytics.ahrefs.com/analytics.js" data-key="0fRSxYXV2FAPynCMexr10w" async></script>
         <script id="chatway" async="true" src="https://cdn.chatway.app/widget.js?id=IccX7SGq0Ppz"></script>      </body>
     </html>
